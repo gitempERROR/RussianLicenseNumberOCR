90fd1fc90f58d994f565d6cab0af6737ecc5da77
gitempERROR <errorfadeev@yandex.ru>
2024-02-11 01:20:35 +0300

    add simpleOCR.py

---
+++ simpleOCR.py
@@ -0,0 +1,37 @@
+import torch
+import torch.nn as nn
+import string
+
+
+class SimpleOCR(nn.Module):
+    def __init__(self, in_channels):
+        super().__init__()
+        self.in_channels = in_channels
+        self.conv = nn.ModuleList(
+            [
+                nn.Conv2d(self.in_channels, 32, kernel_size=3, padding=1),
+                nn.LeakyReLU(0.1),
+                nn.BatchNorm2d(32),
+                nn.MaxPool2d(kernel_size=2, stride=2),
+                nn.Conv2d(32, 64, kernel_size=3, padding=1),
+                nn.LeakyReLU(0.1),
+                nn.BatchNorm2d(64)
+            ]
+        )
+        self.out = nn.ModuleList(
+            [
+                nn.Linear((500 * 100 * 64), 512),
+                nn.LeakyReLU(0.1),
+                nn.Linear(512, 11 * (len(string.digits) + len(string.ascii_letters))),
+                nn.Sigmoid()
+            ]
+        )
+
+    def forward(self, x):
+        for module in self.conv:
+            x = module(x)
+        x = torch.flatten(x, start_dim=1)
+        for module in self.out:
+            x = module(x)
+        return x.reshape(x.shape[0], 11, len(string.digits) + len(string.ascii_letters))
+
