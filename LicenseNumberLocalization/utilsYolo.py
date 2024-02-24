f1a11593285ede8806953b13dd7d5f1af0c5b4e8
gitempERROR <errorfadeev@yandex.ru>
2024-02-24 15:44:57 +0300

    code reformat

--- LicenseNumberLocalization/utilsYolo.py
+++ LicenseNumberLocalization/utilsYolo.py
@@ -3,7 +3,6 @@ from LicenseNumberLocalization import configYolo
 
 
 def intersection_over_union(bboxes1: torch.tensor, bboxes2: torch.tensor, mode='corners'):
-
     try:
         assert mode in ('midpoint', 'width-height', 'corners')
     except Exception:
@@ -13,16 +12,16 @@ def intersection_over_union(bboxes1: torch.tensor, bboxes2: torch.tensor, mode='
     if mode == 'width-height':
         intersection = torch.min(bboxes1[..., 0], bboxes2[..., 0]) * torch.min(bboxes1[..., 1], bboxes2[..., 1])
         union = bboxes1[..., 0] * bboxes1[..., 1] + bboxes2[..., 0] * bboxes1[..., 1] - intersection + 1e-16
-        iou = intersection/union
+        iou = intersection / union
         return iou
 
     # bbox format is [x, y, w, h]
     if mode == 'midpoint':
-        bboxes1[..., 0], bboxes2[..., 0] = bboxes1[..., 0] - bboxes1[..., 2]/2, bboxes2[..., 0] - bboxes2[..., 2]/2
-        bboxes1[..., 1], bboxes2[..., 1] = bboxes1[..., 1] - bboxes1[..., 3]/2, bboxes2[..., 1] - bboxes2[..., 3]/2
+        bboxes1[..., 0], bboxes2[..., 0] = bboxes1[..., 0] - bboxes1[..., 2] / 2, bboxes2[..., 0] - bboxes2[..., 2] / 2
+        bboxes1[..., 1], bboxes2[..., 1] = bboxes1[..., 1] - bboxes1[..., 3] / 2, bboxes2[..., 1] - bboxes2[..., 3] / 2
 
-        bboxes1[..., 2], bboxes2[..., 2] = bboxes1[..., 0] + bboxes1[..., 2]/2, bboxes2[..., 0] + bboxes2[..., 2]/2
-        bboxes1[..., 3], bboxes2[..., 3] = bboxes1[..., 1] + bboxes1[..., 3]/2, bboxes2[..., 1] + bboxes2[..., 3]/2
+        bboxes1[..., 2], bboxes2[..., 2] = bboxes1[..., 0] + bboxes1[..., 2] / 2, bboxes2[..., 0] + bboxes2[..., 2] / 2
+        bboxes1[..., 3], bboxes2[..., 3] = bboxes1[..., 1] + bboxes1[..., 3] / 2, bboxes2[..., 1] + bboxes2[..., 3] / 2
 
         mode = 'corners'
 
@@ -42,6 +41,7 @@ def intersection_over_union(bboxes1: torch.tensor, bboxes2: torch.tensor, mode='
 
     return iou
 
+
 def non_max_suppression(bboxes, iou_threshold, prob_threshold, mode="corners"):
     assert type(bboxes) is list
 
@@ -54,7 +54,7 @@ def non_max_suppression(bboxes, iou_threshold, prob_threshold, mode="corners"):
         bboxes = [
             box for box in bboxes
             if
-            (box[3]*box[4]) / (bboxes_after_nms[-1][4]*bboxes_after_nms[-1][4])
+            (box[3] * box[4]) / (bboxes_after_nms[-1][4] * bboxes_after_nms[-1][4])
             > intersection_over_union(
                 torch.tensor(box[1:]),
                 torch.tensor(bboxes_after_nms[-1][1:]),
@@ -64,12 +64,13 @@ def non_max_suppression(bboxes, iou_threshold, prob_threshold, mode="corners"):
 
     return bboxes_after_nms
 
+
 def get_loaders():
     from datasetYolo import YoloDataset
 
     train_dataset = YoloDataset(
-        image_dir=configYolo.IMAGE_DIR,
-        label_dir=configYolo.LABEL_DIR,
+        dataset_dir=configYolo.DATASET_DIR,
+        label_file=configYolo.LABEL_FILE,
         anchors=configYolo.ANCHORS,
         transforms=configYolo.transforms
     )
@@ -87,8 +88,8 @@ def get_loaders():
 
 def anchor_scaler():
     scaled_anchors = (
-        torch.tensor(configYolo.ANCHORS)
-        * torch.tensor(configYolo.SCALES).unsqueeze(1).unsqueeze(2).repeat(1, 3, 2)
+            torch.tensor(configYolo.ANCHORS)
+            * torch.tensor(configYolo.SCALES).unsqueeze(1).unsqueeze(2).repeat(1, 3, 2)
     ).to(configYolo.DEVICE)
 
     return scaled_anchors
@@ -132,11 +133,11 @@ def bboxes_conversion(bboxes_predictions: torch.tensor, split_size) -> list:
         .unsqueeze(-1)
     ).to(bboxes_predictions.device)
 
-    bboxes_predictions[..., 1:2] = 1/split_size * (bboxes_predictions[..., 1:2] + cell_indices)
+    bboxes_predictions[..., 1:2] = 1 / split_size * (bboxes_predictions[..., 1:2] + cell_indices)
     cell_indices = cell_indices.permute(0, 1, 3, 2, 4)
-    bboxes_predictions[..., 2:3] = 1/split_size * (bboxes_predictions[..., 2:3] + cell_indices)
-    bboxes_predictions[..., 3:5] = 1/split_size *  bboxes_predictions[..., 3:5]
+    bboxes_predictions[..., 2:3] = 1 / split_size * (bboxes_predictions[..., 2:3] + cell_indices)
+    bboxes_predictions[..., 3:5] = 1 / split_size * bboxes_predictions[..., 3:5]
 
-    bboxes_predictions = bboxes_predictions.reshape(batch_size, num_anchors * split_size**2, 5)
+    bboxes_predictions = bboxes_predictions.reshape(batch_size, num_anchors * (split_size ** 2), 5)
 
     return bboxes_predictions.tolist()
