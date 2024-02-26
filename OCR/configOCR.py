66a46c5dab1ed1d143c8b862a012a9874aae83d0
gitempERROR <errorfadeev@yandex.ru>
2024-02-27 01:42:36 +0300

    changes to config params

--- OCR/configOCR.py
+++ OCR/configOCR.py
@@ -1,15 +1,16 @@
 import albumentations as A
 import torch
-import cv2
 from albumentations.pytorch import ToTensorV2
 
 DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
-NUM_WORKERS = 6
-BATCH_SIZE = 16
-LETTER_LIST = ['A', 'B', 'E', 'K', 'M', 'H', 'O', 'P', 'C', 'T', 'Y', 'X', 'r', 'u', 's']
-IMAGE_SIZE = 256
+NUM_WORKERS = 12
+BATCH_SIZE = 8
+LETTER_LIST = ['A', 'B', 'E', 'K', 'M', 'H', 'O', 'P', 'C', 'T', 'Y', 'X']
+IMAGE_SIZE = 144
+IMAGE_HEIGHT = 32
+WEIGHT_DECAY = 1e-4
 LEARNING_RATE = 3e-4
-NUM_EPOCHS = 100
+NUM_EPOCHS = 30
 LOAD_MODEL = True
 SAVE_MODEL = True
 IMAGE_DIR = r'C:\Programming\Datasets\License Plate OCR'
@@ -17,8 +18,7 @@ MODEL_DIR = r'C:\Programming\Models\Vision\OCR_checkpoint.tar'
 
 СONNECT_TRANSFORMS = A.Compose(
     [
-        A.LongestMaxSize(max_size=IMAGE_SIZE),
-        A.PadIfNeeded(min_width=IMAGE_SIZE, min_height=IMAGE_SIZE, border_mode=cv2.BORDER_CONSTANT),
+        A.Resize(height=IMAGE_HEIGHT, width=IMAGE_SIZE),
         A.Normalize(
             mean=0,
             std=1,
@@ -30,16 +30,12 @@ MODEL_DIR = r'C:\Programming\Models\Vision\OCR_checkpoint.tar'
 
 TRANSFORMS = A.Compose(
     [
-        A.LongestMaxSize(max_size=IMAGE_SIZE),
-        A.PadIfNeeded(min_width=IMAGE_SIZE, min_height=IMAGE_SIZE, border_mode=cv2.BORDER_CONSTANT),
-        A.ColorJitter(brightness=0.6, contrast=0.6, saturation=0.5, hue=0.4, p=0.5),
-        A.Blur(p=0.1),
-        A.Affine(translate_percent=0.1, shear=(-45, 45), p=0.5),
-        A.RandomRotate90(p=0.5),
+        A.Resize(height=IMAGE_HEIGHT, width=IMAGE_SIZE, p=1),
+        A.Affine(translate_percent=0.0, shear=(-45, 45), p=0.5),
+        A.ColorJitter(contrast=0.2, always_apply=True),
         A.VerticalFlip(p=0.5),
         A.HorizontalFlip(p=0.5),
         A.Posterize(p=0.1),
-        A.CLAHE(p=0.1),
         A.Normalize(
             mean=0,
             std=1,
