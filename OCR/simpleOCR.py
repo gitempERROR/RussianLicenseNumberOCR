50e1b6c9c4b124bcc584ddedad1f304dff60bdde
gitempERROR <errorfadeev@yandex.ru>
2024-02-27 01:43:26 +0300

    return original model

--- OCR/simpleOCR.py
+++ OCR/simpleOCR.py
@@ -10,32 +10,37 @@ class SimpleOCR(nn.Module):
         self.in_channels = in_channels
         self.conv = nn.ModuleList(
             [
+                # 256x32x144
                 nn.Conv2d(self.in_channels, 32, kernel_size=3, padding=1),
                 nn.ReLU(),
                 nn.BatchNorm2d(32),
                 nn.MaxPool2d(kernel_size=2, stride=2),
+                # 256x16x72
                 nn.Conv2d(32, 64, kernel_size=3, padding=1),
                 nn.Conv2d(64, 64, kernel_size=1),
                 nn.ReLU(),
                 nn.BatchNorm2d(64),
                 nn.MaxPool2d(kernel_size=2, stride=2),
+                # 256x8x36
                 nn.Conv2d(64, 128, kernel_size=3, padding=1),
                 nn.Conv2d(128, 128, kernel_size=1),
                 nn.ReLU(),
                 nn.BatchNorm2d(128),
                 nn.MaxPool2d(kernel_size=2, stride=2),
+                # 256x4x18
                 nn.Conv2d(128, 256, kernel_size=3, padding=1),
                 nn.Conv2d(256, 256, kernel_size=1),
                 nn.ReLU(),
                 nn.BatchNorm2d(256),
                 nn.MaxPool2d(kernel_size=2, stride=2),
+                # 256x2x9
             ]
         )
         self.out = nn.ModuleList(
             [
-                nn.Linear((16 * 16 * 256), 64),
+                nn.Linear((2 * 9 * 256), 64),
                 nn.LeakyReLU(0.1),
-                nn.Linear(64, 12 * (len(string.digits) + len(configOCR.LETTER_LIST)))
+                nn.Linear(64, 9 * (len(string.digits) + len(configOCR.LETTER_LIST)))
             ]
         )
 
@@ -45,5 +50,5 @@ class SimpleOCR(nn.Module):
         x = torch.flatten(x, start_dim=1)
         for module in self.out:
             x = module(x)
-        return x.reshape(x.shape[0], 12, len(string.digits) + len(configOCR.LETTER_LIST))
+        return x.reshape(x.shape[0], 9, len(string.digits) + len(configOCR.LETTER_LIST))
 
